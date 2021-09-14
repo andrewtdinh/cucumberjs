ed6ae57710dbfe4105207ae949934948bd9d66a7
Andrew Dinh <andrewdinh74@yahoo.com>
2021-09-14 15:09:31 -0700

    Finish network unit test

--- test/network.test.js
+++ test/network.test.js
@@ -6,6 +6,13 @@ describe('Network', () => {
   it('broadcasts a message to all listeners', () => {
     const network = new Network();
     const message = "Free bagels!";
+    const lucy = { hear(message) {} }
+    const lucyMock = sinon.mock(lucy);
+
+    lucyMock.expects('hear').once().withArgs(message);
+    network.subscribe(lucy);
+    network.broadcast(message);
+    lucyMock.verify();
 
   })
 })
