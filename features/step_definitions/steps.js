d8485c5a14b5be114063b1d132e2b7ff373dff94
Andrew Dinh <andrewdinh74@yahoo.com>
2021-09-14 15:21:44 -0700

    Update other files

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,9 +1,9 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
-const { assertThat, is } = require('hamjest');
+const { assertThat, is } = require("hamjest");
 
-const { Person, Network } = require('../../src/shouty');
+const { Person, Network } = require("../../src/shouty");
 
-Given('Lucy is {int} meters from Sean', function (distance) {
+Given("Lucy is {int} metres from Sean", function (distance) {
   this.network = new Network();
   this.lucy = new Person(this.network);
   this.sean = new Person(this.network);
@@ -11,11 +11,11 @@ Given('Lucy is {int} meters from Sean', function (distance) {
   this.lucy.moveTo(distance);
 });
 
-When('Sean shouts {string}', function (message) {
+When("Sean shouts {string}", function (message) {
   this.sean.shout(message);
   this.messageFromSean = message;
 });
 
-Then('Lucy hears Sean\'s message', function () {
-  assertThat(this.lucy.messagesHeard(), is([this.messageFromSean]))
+Then("Lucy should hear Sean's message", function () {
+  assertThat(this.lucy.messagesHeard(), is([this.messageFromSean]));
 });
