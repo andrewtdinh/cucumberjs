79ec5bf49fa7c5bc5d753fd45c4adee2f3e71e20
Andrew Dinh <andrewdinh74@yahoo.com>
2021-09-13 17:13:43 -0700

    Fix import error to use require instead

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,18 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+Given('Lucy is located {int} meters from Sean', function (distance) {
+  const lucy = new Person;
+  const sean = new Person;
+  lucy.moveTo(distance);
+  return 'pending';
+});
+
+When('Sean shouts {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('Lucy hears Sean\'s message', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
